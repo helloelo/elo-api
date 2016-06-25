118a2671e094323d19bd4baf042f0484bb36e7bc
Medhi Bechina <medhi.bechina@lazada.com>
2016-06-25 16:59:23 +0700

    Adding match status

--- internal/model/match.go
+++ internal/model/match.go
@@ -6,9 +6,19 @@ import "time"
 type Match struct {
 	IDMatch      uint64    `db:"id_match" json:"id_match"`
 	IDGame       uint64    `db:"fk_game" json:"id_game"`
+	IDStatus     uint64    `db:"id_status" json:"id_status"`
 	Player1      []Player  `db:"player1" json:"player1"`
 	Player2      []Player  `db:"player2" json:"player2"`
 	Player1Score int       `db:"player1_score" json:"player1_score"`
 	Player2Score int       `db:"player2_score" json:"player2_score"`
 	CreatedAt    time.Time `db:"created_at" json:"created_at"`
 }
+
+const (
+	AwaitingConfirmation = 1
+	Declined             = 2
+	Validated            = 3
+	AwaitingApproval     = 4
+	Completed            = 5
+	Invalidated          = 6
+)
