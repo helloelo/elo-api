d11ce9874592ef12ceccf4fde6979761f0b3b82b
Medhi Bechina <medhi.bechina@lazada.com>
2016-06-25 16:59:43 +0700

    Fix notification status, pending state

--- internal/model/notification.go
+++ internal/model/notification.go
@@ -1,10 +1,12 @@
 package model
 
+import "database/sql"
+
 // Notification is the model related to notifications
 type Notification struct {
-	IDNotification     uint64 `db:"id_notification" json:"id_notification"`
-	IDNotificationType int    `db:"fk_notification_type" json:"id_notification_type"`
-	Accepted           bool   `db:"accepted" json:"accepted"`
+	IDNotification     uint64       `db:"id_notification" json:"id_notification"`
+	IDNotificationType int          `db:"fk_notification_type" json:"id_notification_type"`
+	Accepted           sql.NullBool `db:"accepted" json:"accepted"`
 }
 
 const (
